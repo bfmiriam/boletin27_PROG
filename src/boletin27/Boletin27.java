6e81212ec1d82c49032707205f9a7dc2a2185215
sphynx <sphynx@system>
2018-05-01 19:34:09 +0200

    Boletin 27

---
+++ src/boletin27/Boletin27.java
@@ -0,0 +1,14 @@
+
+package boletin27;
+
+
+public class Boletin27 {
+
+   
+    public static void main(String[] args) {
+      
+        Calculadora obx = new Calculadora();
+        obx.setVisible(true);
+    }
+    
+}
